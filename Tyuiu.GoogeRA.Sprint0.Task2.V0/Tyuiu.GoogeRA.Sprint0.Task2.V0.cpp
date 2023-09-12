72c4076708bf195f573655aa5f70e926c1f90c36
Tyuiu.GoogeRA@outlook.com <Tyuiu.GoogeRA@outlook.com>
2023-09-12 14:32:57 +0300

    Task2

--- Tyuiu.GoogeRA.Sprint0.Task2.V0/Tyuiu.GoogeRA.Sprint0.Task2.V0.cpp
+++ Tyuiu.GoogeRA.Sprint0.Task2.V0/Tyuiu.GoogeRA.Sprint0.Task2.V0.cpp
@@ -2,19 +2,13 @@
 //
 
 #include <iostream>
+#include "../Tyuiu.GoogeRA.Sprint0.Task2.V0.Lib/Tyuiu.GoogeRA.Sprint0.Task2.V0.Lib.cpp"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	ISprint0Task2V0* service = new Service();
+	std::cout << "Googe\a\t" << "Robert\t" << "Aleksandrovich\n";
+	std::cout << std::endl;
+	std::cout << "Rezult 1+2= " << service->Add(1, 2);
+	std::cout << std::endl;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
